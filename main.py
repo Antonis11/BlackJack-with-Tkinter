1c2990761623e6819a9865c704586a2dd83d1e5a
Antonis Dichev <dit20152@go.uop.gr>
2025-08-04 11:06:01 +0300

    hand_value()

--- main.py
+++ main.py
@@ -6,6 +6,23 @@ kind = {"♥", "♦", "♠", "♣"}
 
 deck = []
 
+def hand_value(hand):
+    value = 0
+    ace = False
+    for card in hand:
+        if( card[0] == "J" or card[0] == "Q" or card[0] == "K" ):
+            value += 10
+        elif(card[0] == "A"):
+            ace = True
+            value += 1
+        else:
+            value += card[0]
+
+    if ace and value + 10 <= 21:
+        value += 10
+    
+    return value
+
 def main():
 
     window = Tk()
@@ -16,6 +33,8 @@ def main():
     
     deck = [(n, k) for n in number for k in kind]
     shuffle(deck)
+
+    window.mainloop()
     
-    
+
 main()
