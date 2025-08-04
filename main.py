93429630e772925d0e906c142132893f83c22811
Antonis Dichev <dit20152@go.uop.gr>
2025-08-04 11:17:10 +0300

    player() - computer()

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 from random import shuffle
 
 number = {"A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"}
@@ -23,7 +24,109 @@ def hand_value(hand):
     
     return value
 
+def player(window, hand, on_stand_callback):
+    player_hand_frame = Frame(window)
+    player_hand_frame.place(relx=0.5,rely=0.87, anchor=CENTER)
+
+    first = deck.pop()
+    second = deck.pop()
+    hand.add(first)
+    hand.add(second)
+    
+    player = Label(player_hand_frame, text="Player")
+    player.pack(side="top", padx=5)
+
+    cards_frame = Frame(player_hand_frame) 
+    cards_frame.pack(side="top")
+
+    first_card = Label(cards_frame, text=first)
+    first_card.pack(side="left", padx=2)
+
+    second_card = Label(cards_frame, text=second)
+    second_card.pack(side="left", padx=2)
+
+    val = hand_value(hand)
+    value = Label(player_hand_frame, text=f"Value: {val}")
+    value.pack(side="top")
+    
+    if (val == 21):
+        messagebox.showinfo(title="Info message",message="You win!")
+
+    buttonHit = Button(window, text="Hit", command=lambda: clickHit(hand, cards_frame, value))
+    buttonStand = Button(window, text="Stand", command=lambda: clickStand(hand, on_stand_callback, buttonHit, buttonStand))
+    
+    buttonHit.place(relx=0.45, width=50, rely=1.0, anchor="s") 
+    buttonStand.place(relx=0.55, width=50, rely=1.0, anchor="s") 
+
+def clickHit(hand, cards_frame, value):
+    global score
+    another = deck.pop()
+    hand.add(another)
+
+    another_card = Label(cards_frame, text=another)
+    another_card.pack(side="left", padx=2)
+    
+    val = hand_value(hand)
+    value.config(text=f"Value: {val}")
+
+    if (val == 21):
+        messagebox.showinfo(title="Info message",message="You win!")
+    elif (val >= 21):
+        messagebox.showinfo(title="Info message",message="You lose!")
+
+def clickStand(hand, callback, buttonHit, buttonStand):
+    final_value = hand_value(hand)
+    buttonHit.config(state=DISABLED)
+    buttonStand.config(state=DISABLED)
+    callback(final_value)
+
+def computer(window, hand, player_value):
+    computer_hand_frame = Frame(window)
+    computer_hand_frame.place(relx=0.5,rely=0.1, anchor=CENTER)
+
+    first = deck.pop()
+    second = deck.pop()
+    hand.add(first)
+    hand.add(second)
+    
+    computer = Label(computer_hand_frame, text="Computer")
+    computer.pack(side="top", padx=5)
+
+    cards_frame = Frame(computer_hand_frame) 
+    cards_frame.pack(side="top")
+
+    first_card = Label(cards_frame, text=first)
+    first_card.pack(side="left", padx=2)
+
+    second_card = Label(cards_frame, text=second)
+    second_card.pack(side="left", padx=2)
+
+    val = hand_value(hand)
+    value = Label(computer_hand_frame, text=f"Value: {val}")
+    value.pack(side="top")
+
+    while val < player_value:
+        another = deck.pop()
+        hand.add(another)
+
+        another_card = Label(cards_frame, text=another)
+        another_card.pack(side="left", padx=2)
+        
+        val = hand_value(hand)
+        value.config(text=f"Value: {val}")
+        
+    if val > 21:
+        messagebox.showinfo(title="Info message",message="Computer lose!")  
+    elif (val == 21 or val >= player_value):
+        messagebox.showinfo(title="Info message",message="Computer win!")
+
+
+def on_stand(window, final_value):
+        computer_hand = set()
+        computer(window, computer_hand, player_value=final_value)
+
 def main():
+    global deck 
 
     window = Tk()
     window.geometry("500x500")
@@ -34,7 +137,10 @@ def main():
     deck = [(n, k) for n in number for k in kind]
     shuffle(deck)
 
-    window.mainloop()
+    player_hand = set()
+    player(window, player_hand, lambda final_value: on_stand(window, final_value))
     
+    window.mainloop()
+
 
 main()
